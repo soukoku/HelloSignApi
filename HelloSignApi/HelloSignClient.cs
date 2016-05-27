0fb6f37fbabcad43010f9a5c2cc12d442ec34bf7
Eugene Wang <yinchun.wang@gmail.com>
2016-05-26 21:47:29 -0400

    Added api client class.

---
+++ HelloSignApi/HelloSignClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace HelloSignApi
+{
+    /// <summary>
+    /// Main client for performaing HelloSign api calls.
+    /// </summary>
+    public class HelloSignClient
+    {
+        static readonly ConcurrentDictionary<string, HttpClient> __clientCache = new ConcurrentDictionary<string, HttpClient>();
+
+
+        string _apiKey;
+        HttpClient _client;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HelloSignClient" /> class.
+        /// </summary>
+        /// <param name="apiKey">The API key.</param>
+        /// <param name="getHttpClientRoutine">Optional custom routine to provide an <see cref="HttpClient" />.</param>
+        /// <exception cref="ArgumentException">Api key is required.;apiKey</exception>
+        public HelloSignClient(string apiKey, Func<HttpClient> getHttpClientRoutine = null)
+        {
+            if (string.IsNullOrEmpty(apiKey)) { throw new ArgumentException("Api key is required.", "apiKey"); }
+            _apiKey = apiKey;
+
+            if (getHttpClientRoutine != null) _client = getHttpClientRoutine();
+
+            if (_client == null)
+            {
+                _client = __clientCache.GetOrAdd(apiKey, key =>
+                 {
+                     var client = new HttpClient();
+                     return client;
+                 });
+            }
+
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic",
+                Convert.ToBase64String(Encoding.ASCII.GetBytes(apiKey + ":")));
+
+        }
+    }
+}
---
+++ HelloSignApi/Internal/ResponseExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloSignApi
+{
+    static class ResponseExtensions
+    {
+        internal static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new UnderscoreMappingResolver()
+        };
+
+        public static Task<T> ParseApiResponseAsync<T>(this HttpResponseMessage msg) where T : ApiResponse
+        {
+            return msg.Content.ReadAsStringAsync().ContinueWith(t =>
+            {
+                var json = t.Result;
+
+                var model = JsonConvert.DeserializeObject<T>(json, JsonSettings);
+                if (model == null) { model = Activator.CreateInstance<T>(); }
+
+                model.FillExtraValues(msg);
+
+                return model;
+            });
+        }
+    }
+}
