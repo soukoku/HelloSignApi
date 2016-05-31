25e223105af5d5a2f67b33f79900ce0c730fabb1
Eugene Wang <yinchun.wang@gmail.com>
2016-05-31 05:28:05 -0400

    Added template doc model.

--- HelloSignApi/Models.cs
+++ HelloSignApi/Models.cs
@@ -342,7 +342,61 @@ namespace HelloSignApi
     /// </summary>
     public class Document
     {
-        // TODO: add properties
+        /// <summary>
+        /// Name of the associated file
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Document ordering, the lowest index is diplayed first and the highest last.
+        /// </summary>
+        public int Index { get; set; }
+        /// <summary>
+        /// An array of Form Field objects containing the name and type of each named textbox and checkmark field.
+        /// </summary>
+        public FormField[] FormFields { get; set; }
+        /// <summary>
+        /// An array of Custom Field objects containing the name and type of each custom field.
+        /// </summary>
+        public CustomField[] CustomFields { get; set; }
+    }
+
+    /// <summary>
+    /// Field object for a template <see cref="Document"/>.
+    /// </summary>
+    public class FormField
+    {
+        /// <summary>
+        /// A unique id for the form field.
+        /// </summary>
+        public string ApiId { get; set; }
+        /// <summary>
+        /// The name of the form field.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The type of this form field. See <see cref="FieldTypes"/> values.
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// The horizontal offset in pixels for this form field.
+        /// </summary>
+        public int X { get; set; }
+        /// <summary>
+        /// The vertical offset in pixels for this form field.
+        /// </summary>
+        public int Y { get; set; }
+        /// <summary>
+        /// The width in pixels of this form field.
+        /// </summary>
+        public int Width { get; set; }
+        /// <summary>
+        /// The height in pixels of this form field.
+        /// </summary>
+        public int Height { get; set; }
+        /// <summary>
+        /// Boolean showing whether or not this field is required.
+        /// </summary>
+        public bool Required { get; set; }
     }
 
     /// <summary>
