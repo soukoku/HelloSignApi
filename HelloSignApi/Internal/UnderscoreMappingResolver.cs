909a7a14098a1ec59b30223e6995fef2c90d9891
Eugene Wang <yinchun.wang@gmail.com>
2016-06-06 06:17:51 -0400

    Used more explicit package names for net standard.

--- HelloSignApi/Internal/UnderscoreMappingResolver.cs
+++ HelloSignApi/Internal/UnderscoreMappingResolver.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace HelloSignApi
 {
@@ -12,7 +13,7 @@ namespace HelloSignApi
     {
         protected override string ResolvePropertyName(string propertyName)
         {
-            return System.Text.RegularExpressions.Regex.Replace(
+            return Regex.Replace(
                 propertyName, @"([A-Z])([A-Z][a-z])|([a-z0-9])([A-Z])", "$1$3_$2$4").ToLower();
         }
     }
